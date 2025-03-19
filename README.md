ac61b14ba63ed9e4c5e104eb256845279598d0ff
Adrian Gidaro <adriangidaro@gmail.com>
2025-03-19 20:55:17 +1100

    Finished user model, bp; tested CRUD routes

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # hodl-server
 DEV1002 - Databases and Servers - Assessment 3
+
+
+# References
+https://docs.python.org/3/library/datetime.html
+https://dev.to/jakewitcher/using-env-files-for-environment-variables-in-python-applications-55a1
+https://www.geeksforgeeks.org/how-to-create-and-use-env-files-in-python/ 
+https://www.psycopg.org/docs/errors.html 
--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 from flask import Flask
 from init import db, ma
 from dotenv import load_dotenv
+from marshmallow.exceptions import ValidationError
 import os
+from blueprints.db_bp import db_bp
+from blueprints.users_bp import users_bp
 
 def create_app():
     app = Flask(__name__)
     
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
+    load_dotenv()
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI')
+    
+    # If a validation error happens anywhere in the application, this will be returned
+    @app.errorhandler(ValidationError)
+    def validation_error(err):
+        return {"Error": str(err)}, 400
     
     db.init_app(app)
     ma.init_app(app)
+    
+    app.register_blueprint(db_bp)
+    app.register_blueprint(users_bp)
     
     return app
